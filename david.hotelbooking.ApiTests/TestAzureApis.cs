3bce9a7d8cd82120bb87e06ddc6eb0d7a497801a
daviddongguo <david.dong.guo@gmail.com>
2020-10-07 16:17:15 -0400

    add new project for testing api by end user

---
+++ david.hotelbooking.ApiTests/TestAzureApis.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using RestSharp;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace david.hotelbooking.ApiTests
+{
+    [TestFixture]
+    public class TestAzureApis
+    {
+        private IClient _client;
+        private readonly string baseUsrl = "https://davidwuhotelbooking.azurewebsites.net/";
+        [SetUp]
+        public void SetUp()
+        {
+            _client = new RestClient(baseUsrl);
+        }
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            var answer = 42;
+            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+        }
+    }
+}
