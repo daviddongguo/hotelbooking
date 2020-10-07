97ae3234984c483e52ae2f8eb307a5bd414d894c
daviddongguo <david.dong.guo@gmail.com>
2020-10-07 17:22:23 -0400

    update mysql connection string

--- david.hotelbooking.UnitTests/Services/RemoteMySqlDbcontextFactory.cs
+++ david.hotelbooking.UnitTests/Services/RemoteMySqlDbcontextFactory.cs
@@ -10,7 +10,7 @@ namespace david.hotelbooking.UnitTests.Services
         public UserDbContext GetUserContext()
         {
             var options = new DbContextOptionsBuilder<UserDbContext>()
-                        .UseMySql("server=bd8qkqg3jffjiwkgnlyc-mysql.sfervices.clever-cloud.com;user=uriilvzrrptwkye2;password=qODCkgKQu7gWzEwGb4z6;database=bd8qkqg3jffjiwkgnlyc",
+                        .UseMySql("server=bd8qkqg3jffjiwkgnlyc-mysql.services.clever-cloud.com; user=uriilvzrrptwkye2;password=qODCkgKQu7gWzEwGb4z6;database=bd8qkqg3jffjiwkgnlyc",
                         op => op.EnableRetryOnFailure())
                         .Options;
             var dbContext = new UserDbContext(options);
