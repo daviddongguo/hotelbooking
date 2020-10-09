dbe69c8a4913d806b2cd32e2da63b1d5b602f170
daviddongguo <david.dong.guo@gmail.com>
2020-10-09 17:03:35 -0400

    upgrade user roles editing page by using table and form

---
+++ david.hotelbooking.mvc/Controllers/PermissionsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using david.hotelbooking.domain.Concretes;
+using david.hotelbooking.domain.Entities.RBAC;
+
+namespace david.hotelbooking.mvc.Controllers
+{
+    public class PermissionsController : Controller
+    {
+        private readonly UserDbContext _context;
+
+        public PermissionsController(UserDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Permissions
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Permissions.ToListAsync());
+        }
+
+        // GET: Permissions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var permission = await _context.Permissions
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (permission == null)
+            {
+                return NotFound();
+            }
+
+            return View(permission);
+        }
+
+        // GET: Permissions/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Permissions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description")] Permission permission)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(permission);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(permission);
+        }
+
+        // GET: Permissions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var permission = await _context.Permissions.FindAsync(id);
+            if (permission == null)
+            {
+                return NotFound();
+            }
+            return View(permission);
+        }
+
+        // POST: Permissions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description")] Permission permission)
+        {
+            if (id != permission.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(permission);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PermissionExists(permission.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(permission);
+        }
+
+        // GET: Permissions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var permission = await _context.Permissions
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (permission == null)
+            {
+                return NotFound();
+            }
+
+            return View(permission);
+        }
+
+        // POST: Permissions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var permission = await _context.Permissions.FindAsync(id);
+            _context.Permissions.Remove(permission);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PermissionExists(int id)
+        {
+            return _context.Permissions.Any(e => e.Id == id);
+        }
+    }
+}
