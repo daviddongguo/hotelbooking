c28a345312b7fbd07c76d85672829d51aabd13e9
daviddongguo <david.dong.guo@gmail.com>
2020-10-10 23:36:24 -0400

    using autofac

--- david.hotelbooking.api/Program.cs
+++ david.hotelbooking.api/Program.cs
@@ -1,20 +1,33 @@
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
 
 namespace david.hotelbooking.api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            // The `UseServiceProviderFactory(new AutofacServiceProviderFactory())` call here allows for
+            // ConfigureContainer to be supported in Startup with
+            // a strongly-typed ContainerBuilder. If you don't
+            // have the call to AddAutofac here, you won't get
+            // ConfigureContainer support. This also automatically
+            // calls Populate to put services you register during
+            // ConfigureServices into Autofac.
+            var host = Host.CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureWebHostDefaults(webHostBuilder => webHostBuilder.UseStartup<Startup>())
+                .Build();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+            await host.RunAsync();
+        }
+        //public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //    Host.CreateDefaultBuilder(args)
+        //        .ConfigureWebHostDefaults(webBuilder =>
+        //        {
+        //            webBuilder.UseStartup<Startup>();
+        //        });
     }
 }
