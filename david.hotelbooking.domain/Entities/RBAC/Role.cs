4f94779adb95b91347f7180fa96860848dc65505
daviddongguo <david.dong.guo@gmail.com>
2020-10-12 11:20:34 -0400

    UpGrade User Design by add Group

--- david.hotelbooking.domain/Entities/RBAC/Role.cs
+++ david.hotelbooking.domain/Entities/RBAC/Role.cs
@@ -7,10 +7,12 @@ namespace david.hotelbooking.domain.Entities.RBAC
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
-        public bool IsActive { get; set; }
+        public bool IsActive { get; set; } = true;
 
+        public virtual ICollection<GroupRole> GroupRoles { get; set; }
         public virtual ICollection<UserRole> UserRoles { get; set; }
         public virtual ICollection<RolePermission> RolePermissions { get; set; }
     }
