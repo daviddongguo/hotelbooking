bd586b4df378b19ec85ad3f9c913ac8ed8e47655
daviddongguo <david.dong.guo@gmail.com>
2020-10-12 21:02:52 -0400

    Create Hotel Models

---
+++ david.hotelbooking.domain/Entities/Hotel/Guest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace david.hotelbooking.domain.Entities.Hotel
+{
+    public class Guest
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public virtual ICollection<Booking> Bookings { get; set; }
+    }
+}
